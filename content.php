9b43e89a4319673d51341e57b0448581745e3b79
addinall <addinall@addinall.net>
2014-10-09 01:12:42 +1000

    First commit after restructure

---
+++ content.php
@@ -0,0 +1,135 @@
+
+<?php
+// vim: set expandtab tabstop=4 shiftwidth=4 autoindent smartindent:
+//---------------------------------------------------------
+// CAPTAIN  SLOG
+//---------------------------------------------------------
+//
+//	FILE:       content.php 
+//	SYSTEM:     Ridolfis' new system 
+//	            WAS.  He didn't want it so I am stealing it for myself!
+//	AUTHOR:     Mark Addinall
+//	DATE:       20/08/2014
+//	SYNOPSIS:   Instead of burdening a simple little elegant system with a 
+//              BIG FAT UGLY waste of time like JOOMLA!, DJANGO or WordPress,
+//              we just stick in some content here that can be easily managed
+//              with a TEXT EDITOR.
+//
+//              **** DO NOT USE WORD!!! ******
+//
+//------------+-------------------------------+------------
+// DATE       |    CHANGE                     |    WHO
+//------------+-------------------------------+------------
+//
+//
+
+
+
+// First the header conten
+
+$logo       = "Mark Addinall";
+$logo2      = "That&#8217;s IT!";
+
+$header_message = <<<END
+"This site represents addinall.org.  Much the same information as addinall.com.au, just a different type of web page.  This is one of the styles I have been working on.  Single page, responsive.  Built for mobile device use.  I hope you like it."
+END;
+
+// now before anyone starts screaming "USE AN ARRAY, OR AN OBJECT CONTAINING AN ARRAY, OR AN
+// ARRAY OF OBJECTS",  I am not giving mu customer lessons in CompSci.  I want him, her or
+// to easily understand that if you changed the text in paragraph one, it will reflect
+// on the screen in paragraph one.  That is ALL I want this file to do.  So fuck off, weenies.
+
+$header_paragraph = <<<END
+Thanks for visiting my site.  Before we go anywhere, I have to make a point so as not to dissapoint.  <em>I DO NOT USE PHP MVC Frameworks.</em>  I know how they work, I have used all of them and I think that every last one of the clunkers are worse than rubbish.  So if you have a CodeIgniter, CakePHP, Symfony, Xii or Zend Framework task in mind, look ye elsewhere.  Seriously, the dozen or so projects I have worked on that tried to build an application using one of these systems all went the same way.  First three weeks were just <em>DANDY!</em>  Then when the application started to get complex, the design fell in a heap, leaving non-working, un-maintainable code. So no thanks.  I also <em>NO LONGER</em> do Java (love Javascript) because it is fat, old, slow and ugly.  I don&#8217;t do Haskell, Ruby, Scala, C# or F# for very similar reasons.  Now that we have covered what I <em>DON&#8217;T</em> do, let&#8217;s have a look at what I <em>DO</em> do! 
+END;
+
+// This is the END of the HEADER CONTENT
+
+// These are the various HEADINGS in the system
+// If you want to INCREASE the number of paragraphs, you need to understand index.php
+
+$sidebar_header     = "Contact me!";
+$heading_one        = "PHP OOD/OOP Development";
+$heading_two        = "Modern Perl Development";
+$heading_three      = "HTML5 - CSS3 - Javascript Development";
+$heading_four       = "Heaps of OTHER STUFF!";
+
+$footer_heading = "I hope you had a nice visit.  Come back soon.";
+
+// This is the END of the HEADINGS
+// If you want to get rid of one or more, make them an empty string = "";
+// If you want to add more paragraphs, that is more complex.  Ask first!  ;-)
+
+
+// This is the START of the SIDEBAR and MAIN CONTENT
+
+
+$sidebar_paragraph = <<<END
+The stuff down below is some music I have gathered to have a play with the HTML5 audio object.  With luck, the music <em>SHOULD</em> play on any device (if I have my code correct that is!).  I am supposed to cut some of my own tracks for here using the <em>BRAND NEW Gibson SG!!!!</em>  I will very shortly Darling Annie!!!
+END;
+
+$paragraph_one = <<<END
+PHP has been my major weapon of choice for over a decade now.  It is a simple and elegant little language that is <em>MESSED UP</em> on a regular basis by people trying to implement MVC garbage.  When used properly PHP allows us to develop modular, reliable and easy to maintain source code for applications small and large. I would think that PHP represent around 75% of my total work to date. 
+END;
+
+$paragraph_two = <<<END
+I <em>LOVE</em> Perl!  I had the opportunity to code a small system just recently for OzLotto that allowed me to use Modern Perl.  Perl is already fantastic.  The Moose Libraries just make it a whole heap (sic) better. I am using Modern Perl (and R) in some Bioinformatics studies I am currently undertaking.  Good fun! 
+END;
+
+$paragraph_three = <<<END
+<p>Goes without saying really, but unless you spell it out, the people that infest the HR departments of the Yooniverse get all confused, twisted and bitter.  I am well experienced in HTML (of all flavours, I concentrate on HTML5 these days), CSS3, responsive Web design for mobile devices. I am having a lot of fun learning and exploring the CSS3 transitions and animations.  Some play code is <a href="http://www.addinall.net/boxmenu" target="_blank">HERE</a>.  Having a play and a think about how more interactive choices could be made in a UI design!</p>
+    <p>Again I can not see how anyone could AVOID Javascript.  I have had several years coding both native Javascript and jQuery framework.  I enjoy them both. </p>
+<p>Here are a couple of sites/apps I finished recently. <a href="http://www.crosscitydjs.com.au" target="_blank">Cross City DJs</a> and <a href="http://www.crosscitydjs.com.au/eportfolio" target="_blank">Same customer, newer page</a>.   
+END;
+
+$paragraph_four = <<<END
+<p>
+I have <em>ALWAYS</em> been the administrator of my development machines.  In a number of roles I have been hired purely as a systems administrator.  My skills include UNIX (Solaris, AIX, BSD, HP-UX), Linux (Red Hat, Debian, Fedora, UBUNTU, Centos), VMWare, VirtualBox, BASH, ksh, Perl, Python, monitoring, patch administration, backup implementation, operation, testing and recovery. 
+</p>
+<p>
+Very experienced SQL coder in many flavours.  They are all pretty much a muchness.  I spent a good deal of time coding in ORACLE PL/SQL, most notably for the Australian Bureau of Statistics on the new (2006) implementation of the population demographic collection and analysis software. 
+</p>
+END;
+
+$footer_paragraph = <<<END
+<p>This is one page of two at the moment.  You may have seen my other page just now!  If not, it is a more traditional shape and contains a lot more links to my work.  <a href="http://www.addinall.com.au">addinall.com.au</a>. </p>
+<p>
+I completed degrees (post grad) in engineering and IT in the past. The last attendence in person being 1992.  So possibly no longer relevant.  Details in my <a href="../Mark_Addinall_cv_plain.pdf" target="_blank">LONG CV!</a>  Recently I have been making use of Web based distance education a great deal.  A link to some of my recent results can be found <a href="http://www.addinall.com.au/Recent_results/compile.pdf" target="_blank">HERE</a>.  I am committed so far to another two years consisting of Biology, Epidemiology and Education studies.  I have an idea that my IT future will probably be either in the Life Sciences or Education fields.  I am still interested in mainstream coding.  Looking at Bioinformatic and Educational software tells me that they need help; right now! The ability to be creative in a position very welcome. Open for offers! 
+</p>
+<p>When I am not coding or studying, I surf, play guitar and sometimes ride a pushie.  I drive a white truck!</p>
+END;
+
+
+// This next bit is in here because it is fucking ugly as, but really, really important.
+// I would suggest that if you are the end customer, NOT to experiment with this
+// lot.  Just assume it works and don't poke it.
+
+$footer_legals = <<<END
+&copy; 2014 <span class="license">Created by <a href="http://www.addinall.net/contact.php" target="_blank"><em>Mark Addinall</em></a>  Using Chameleon V5.0 Web Toolset.</span>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+END;
+
+
+$footer_javascript = <<<END
+    <!-- Minimized jQuery from Google CDN -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.6.2/jquery.min.js"></script>
+    <!-- HTML5 IE Enabling Script -->
+    <!--[if lt IE 9]><script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script><![endif]-->
+    <!-- CSS3 Media Queries -->
+    <script src="js/respond.min.js"></script>
+    <!-- Optimized Google Analytics. Change UA-XXXXX-X to your site ID -->
+    <script>var _gaq=[['_setAccount','UA-XXXXX-X'],['_trackPageview']];(function(d,t){var g=d.createElement(t),s=d.getElementsByTagName(t)[0];g.src='//www.google-analytics.com/ga.js';s.parentNode.insertBefore(g,s)}(document,'script'))</script>
+END;
+
+
+// here at the wanky things to say about yourself, music
+//
+
+
+$wank_1 = "dedicated";
+$wank_2 = "enthusiastic";
+$wank_3 = "creative";
+$wank_4 = "energetic";
+$wank_5 = "meticulous";
+$wank_6 = "innovative";
+
+?>
---
+++ index.php
@@ -0,0 +1,221 @@
+<!DOCTYPE html>
+<html lang="en">
+<!--
+ vim: set expandtab tabstop=4 shiftwidth=4 autoindent smartindent:
+// CAPTAIN  SLOG
+//
+//  FILE:       index.php 
+//  SYSTEM:     New page for Ridolfi 
+//              It was, he didn't want ti so I stole it for myself
+//              to host addinall.org and leave my other page
+//              hosting addinall.com.au
+//  AUTHOR:     Mark Addinall
+//  DATE:       19/08/2014
+//  SYNOPSIS:   In the words of Ray Charles, "an I guess I'll take
+                another IOU...."
+
+    This page in all of it's manifistations is RESPONSIVE and FLUID.
+    It will work on a PC, Fondleslab or telephone.  Perhaps not a
+    Rotary Telephone.
+
+    Everything in this site is built responsive from the ground up,
+    the slider, the forms, image containers, movie containers.
+
+    20/08/2014 -    Changed the file from .html to .php to indicate that we will
+                    now be managing the content through some VERY simplistic
+                    PHP inline code.  This gives Apache for warning to load the
+                    PHP modules, speeding processing.
+    21/08/2014 -    Looks pretty much finished to me.  CSS is pretty hoopy!  That
+                    was a 37 hour shift!!!!  Too tired to sleep!!
+-->
+
+<head prefix="og: http://ogp.me/ns# fb: http://ogp.me/ns/fb# website: http://ogp.me/ns/website#">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0"> 
+<meta name="description" content="A web site created for me.">
+<meta name="keywords"  content="HTML5, CSS3, PHP, Responsive, Fluid, Web, Design, Modern">
+<meta name="author" content="Mark Addinall.  Brisbane. Australia">
+<meta name="robots" content="all">
+
+<title>Mark Addinall - addinall.org</title>
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1"/>
+
+
+    
+<!-- Place favicon.ico and apple-touch-icon.png in root directory -->
+        
+<link href="css/style.css" rel="stylesheet" />
+<link href="css/lawrence.css" rel="stylesheet" />
+
+<link rel="stylesheet" type="text/css" href="css/style2.css" />
+<link rel="stylesheet" href="AudioPlayer/css/audioplayer.css" />
+
+<script src="AudioPlayer/js/jquery.js"></script>
+<script src="AudioPlayer/js/audioplayer.js"></script>
+
+<script>$( function() { $( 'audio' ).audioPlayer(); } );</script>
+
+<script type="text/javascript" src="js/modernizr.custom.86080.js"></script>
+
+
+<?php require("content.php");   ?>
+
+
+
+</head>
+
+<body id="page">
+    <ul class="cb-slideshow">
+        <li><span>Image 01</span><div><h3><?php echo $wank_1; ?></h3></div></li>
+        <li><span>Image 02</span><div><h3><?php echo $wank_2; ?></h3></div></li>
+        <li><span>Image 03</span><div><h3><?php echo $wank_3; ?></h3></div></li>
+        <li><span>Image 04</span><div><h3><?php echo $wank_4; ?></h3></div></li>
+        <li><span>Image 05</span><div><h3><?php echo $wank_5; ?></h3></div></li>
+        <li><span>Image 06</span><div><h3><?php echo $wank_6; ?></h3></div></li>
+    </ul>
+
+    <header>
+        <div class="g1">
+            <h1><?php echo $logo;  ?></h1>
+            <h2><?php echo $logo2;  ?></h2>
+        </div>
+        <div class="g2">
+            <p></p>
+            <h5>
+            <?php echo $header_message; ?>
+            </h5>
+        </div>
+        <div class="g2">
+            <p>
+            <?php echo $header_paragraph; ?>
+            </p>
+        </div>
+    </header>
+    <div class="cf"></div>
+    <div id="content">
+        <div class="g1">
+            <h3><?php echo '<a href="http://www.addinall.com.au/contact.php" target="_blank">' . $sidebar_header . "</a>"; ?></h3>
+            <p>
+                <?php echo $sidebar_paragraph; ?>
+            </p>
+
+    <!-- That was the end of the actual HUMAN editable content for the sidebar.
+        In this installation the rest of this sidebar is full of AUDIO machines.
+        It might not be a great idea to edit them unless you are pretty sure
+        of what is going on!  -->
+
+<!--
+// MUSIC.  Now, you have five slots.  If you need more we need to think about the
+// screen real estate and geometry, and not just slap things in willy-nilly.
+// To make the music compatible over ALL browsers and operating system you
+// should take the time to make OGG and WAV audio files as well as MP3.
+// Not all browsers will play an MP3.  You be like "I'm an audio engineer, I can do dat"
+//
+// here is a good place to do the converts.  They ask for a $1.90 donation.
+// I am good with that.  You can upload all at the same time in a batch and the thing
+// works (which makes a change).
+//
+// http://media.io/
+-->
+            <h5>Juicy</h5>
+            <p>
+                <audio preload="none" controls>
+                    <source src="AudioPlayer/audio/juicy.mp3">
+                    <source src="AudioPlayer/audio/juicy.ogg">
+                    <source src="AudioPlayer/audio/juicy.wav">
+                </audio>
+            </p>
+            <h5>Gun - ZHU</h5>
+            <p>
+                <audio preload="none" controls>
+                    <source src="AudioPlayer/audio/zhu.mp3"> 
+                    <source src="AudioPlayer/audio/zhu.ogg"> 
+                    <source src="AudioPlayer/audio/zhu.wav"> 
+                </audio>
+            </p>
+            <h5>Thirsty Eye</h5>
+            <p>
+                <audio preload="none" controls>
+                    <source src="AudioPlayer/audio/thirsty.mp3"> 
+                    <source src="AudioPlayer/audio/thirsty.ogg"> 
+                    <source src="AudioPlayer/audio/thirsty.wav"> 
+                </audio>
+            </p>
+            <h5>Pickles from the Jar</h5>
+            <p>
+                <audio preload="none" controls>
+                    <source src="AudioPlayer/audio/pickles.mp3"> 
+                    <source src="AudioPlayer/audio/pickles.ogg"> 
+                    <source src="AudioPlayer/audio/pickles.wav"> 
+                </audio>
+            </p>
+            <h5>All Night</h5>
+            <p>
+	            <audio preload="none" controls>
+		            <source src="AudioPlayer/audio/night.mp3">
+		            <source src="AudioPlayer/audio/night.ogg">
+		            <source src="AudioPlayer/audio/night.wav">
+	            </audio>
+            </p>
+            <h5>Cross Eyed Woman</h5>
+            <p>
+	            <audio preload="none" controls>
+		            <source src="AudioPlayer/audio/cross.mp3">
+		            <source src="AudioPlayer/audio/cross.ogg">
+		            <source src="AudioPlayer/audio/cross.wav">
+	            </audio>
+            </p>
+            <h5>Death Grips</h5>
+            <p>
+	            <audio preload="none" controls>
+		            <source src="AudioPlayer/audio/death.mp3">
+		            <source src="AudioPlayer/audio/death.ogg">
+		            <source src="AudioPlayer/audio/death.wav">
+	            </audio>
+            </p>
+        </div>
+
+        <!-- That is the end of the side bar.  REMEMBER, this is a FLUID and
+             RESPONSIVE GRID system that will allocate space as it sees available.
+             The bit we just finished behaves as a LEFT SIDE BAR on full size
+             system!!!   -->
+
+
+        <div class="g2">
+            <h3><?php echo $heading_one; ?></h3>
+            <p>
+                <?php echo $paragraph_one; ?>
+            </p>
+            <h3><?php echo $heading_two; ?></h3>
+            <p>
+                <?php echo $paragraph_two; ?>
+            </p>
+            <h3><?php echo $heading_three; ?></h3>
+            <p>
+                <?php echo $paragraph_three; ?>
+            </p>
+            <h3><?php echo $heading_four; ?></h3>
+            <p>
+                <?php echo $paragraph_four; ?>
+            </p>
+        </div>
+        <div class="g3">
+            <h3><?php echo $footer_heading; ?></h3>
+            <p>
+                <?php echo $footer_paragraph; ?>
+            </p>
+        </div>
+    </div>
+    <footer class="g3 cf">
+        <hr>
+            <?php echo $footer_legals; ?>
+        <hr>
+    </footer>
+
+    <!-- JavaScript at the bottom for fast page loading -->
+
+    <?php echo $footer_javascript; ?>
+
+</body>
+</html>
